c96ea28e675af635c57351ed9a29da6d9e7830ab
Loki <loki-47-6F-64@users.noreply.github.com>
2015-10-10 21:07:36 +0200

    add a wait for debugger routine

--- src/game/game.s
+++ src/game/game.s
@@ -21,31 +21,77 @@ along with gamelib-x64. If not, see <http://www.gnu.org/licenses/>.
 
 .global gameInit
 .global gameLoop
+.global wait_for_debugger
+
+.global panic
 
 .section .data
 
 .section .text
 
+/*
+  Simple infinite loop
+  The debugger should nop the instructions
+*/
+wait_for_debugger:
+  hlt
+  jmp wait_for_debugger
+  ret
+
 gameInit:
-	ret
+  jmp c_init
 
 gameLoop:
-	# Check if a key has been pressed
-	call	readKeyCode
-	cmpq	$0, %rax
-	je		1f
-	# If so, print a 'Y'
-	movb	$'Y', %dl
-	jmp		2f
+  jmp c_loop
 
-1:
-	# Otherwise, print a 'N'
-	movb	$'N', %dl
+/*
+  Something unrecoverable happened
+  We must print as much as possible on the screen before halting
+*/
+panic:
+  pushq %r15
+  pushq %r14
+  pushq %r13
+  pushq %r12
+  pushq %r11
+  pushq %r10
+  pushq %r9
+  pushq %r8
 
-2:
-	movq	$0, %rdi
-	movq	$0, %rsi
-	movb	$0x0f, %cl
-	call	putChar
+  # push rsp with original value
+  movq %rsp, %r15
+  addq $64, %r15
+  pushq %r15
 
-	ret
+  pushq %rbp
+  pushq %rdi
+  pushq %rsi
+  pushq %rdx
+  pushq %rcx
+  pushq %rbx
+  pushq %rax
+
+  movq %rsp, %rdi
+  call print_snapshot
+panic_hlt:
+  hlt
+  jmp panic_hlt
+//	# Check if a key has been pressed
+//	call	readKeyCode
+//	cmpq	$0, %rax
+//	je		1f
+//	# If so, print a 'Y'
+//	movb	$'Y', %dl
+//	jmp		2f
+//
+//1:
+//	# Otherwise, print a 'N'
+//	movb	$'N', %dl
+//
+//2:
+//	movq	$0, %rdi
+//	movq	$0, %rsi
+//	movb	$0x0f, %cl
+//	call	putChar
+//
+//	ret
