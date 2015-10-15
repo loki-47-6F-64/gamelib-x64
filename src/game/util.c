a8eb0f385aa1b81b98abc629e347da0083e72454
Loki <loki-47-6F-64@users.noreply.github.com>
2015-10-15 12:18:44 +0200

    use some subroutines creating for assignment 5b

--- src/game/util.c
+++ src/game/util.c
@@ -16,72 +16,72 @@ void fill(void *out, uint8_t val, uint64_t count) {
 }
 
 
-// returns ascii representation of the value
-char fromDigit(char val) {
-  return val + '0';
-}
-
-/*
- * str  -- array to be reversed
- * size -- size of the array
- */
-void reverse(char *str, uint64_t size) {
-  char *backward_it = str + size -1;
-  while(str < backward_it) {
-    char tmp = *backward_it;
-    *backward_it = *str;
-    *str = tmp;
-
-    ++str;
-    --backward_it;
-  }
-}
-
-/* convert 'in' into a string
- *
- * out -- output for the string
- * in -- input for the string
- *
- * return:
- *  size of generated string
- */
-uint64_t int_to_string(char *out, int64_t in) {
-  if(in >= 0) {
-    return uint_to_string(out, in); 
-  }
-
-  in = (~in) + 1; // get positive value
-  *out = '-';
-
-
-  return uint_to_string(out +1, in) +1;
-}
-
-/* convert 'in' into a string
- *
- * out -- output for the string
- * in -- input for the string
- *
- * return:
- *  size of generated string
- */
-uint64_t uint_to_string(char *out, uint64_t in) {
-  if(in == 0) {
-    *out = fromDigit(in);
-
-    return 1;
-  }
-
-  char *out_p = out;
-  while(in) {
-    *out_p++ = fromDigit(in % 10);
-    in /= 10;
-  }
-
-  uint64_t size = out_p - out;
-  reverse(out, size);
-  return size;
-}
+// // returns ascii representation of the value
+// char fromDigit(char val) {
+//   return val + '0';
+// }
+// 
+// /*
+//  * str  -- array to be reversed
+//  * size -- size of the array
+//  */
+// void reverse(char *str, uint64_t size) {
+//   char *backward_it = str + size -1;
+//   while(str < backward_it) {
+//     char tmp = *backward_it;
+//     *backward_it = *str;
+//     *str = tmp;
+// 
+//     ++str;
+//     --backward_it;
+//   }
+// }
+// 
+// /* convert 'in' into a string
+//  *
+//  * out -- output for the string
+//  * in -- input for the string
+//  *
+//  * return:
+//  *  size of generated string
+//  */
+// uint64_t int_to_string(char *out, int64_t in) {
+//   if(in >= 0) {
+//     return uint_to_string(out, in); 
+//   }
+// 
+//   in = (~in) + 1; // get positive value
+//   *out = '-';
+// 
+// 
+//   return uint_to_string(out +1, in) +1;
+// }
+// 
+// /* convert 'in' into a string
+//  *
+//  * out -- output for the string
+//  * in -- input for the string
+//  *
+//  * return:
+//  *  size of generated string
+//  */
+// uint64_t uint_to_string(char *out, uint64_t in) {
+//   if(in == 0) {
+//     *out = fromDigit(in);
+// 
+//     return 1;
+//   }
+// 
+//   char *out_p = out;
+//   while(in) {
+//     *out_p++ = fromDigit(in % 10);
+//     in /= 10;
+//   }
+// 
+//   uint64_t size = out_p - out;
+//   reverse(out, size);
+//   return size;
+// }
 
 const char _bits[16] = {
   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
@@ -104,29 +104,36 @@ void uint_to_hex(char *out, uint64_t in) {
 
   reverse(out, 16);
 }
-
-
-/**
- * str -- the string to return the size of
- */
-uint64_t strlen(const char *str) {
-  uint64_t size = 0;
-  while(*str++) {
-    ++size;
-  }
-
-  return size;
-}
-
-/*
- * copy null-terminated string from 'in' to 'out'
- * 'out' will not be null-terminated
- */
-uint64_t strcpy(char *out, const char *in) {
-  char *out_p = out;
-  while(*in) {
-    *out_p++ = *in++;
-  }
-
-  return out_p - out;
-}
+// 
+// 
+// /**
+//  * str -- needs to be null-terminated
+//  * return
+//  *  the size of the string
+//  */
+// uint64_t strlen(const char *str) {
+//   uint64_t size = 0;
+//   while(*str++) {
+//     ++size;
+//   }
+// 
+//   return size;
+// }
+// 
+// /* copy null-terminated string from 'in' to 'out'
+//  * 'out' will not be null-terminated
+//  *
+//  * params:
+//  *  out -- the destination of the copy
+//  *  in  -- the source of the copy
+//  * return:
+//  *  the amount of characters copied
+//  */
+// uint64_t strcpy(char *out, const char *in) {
+//   char *out_p = out;
+//   while(*in) {
+//     *out_p++ = *in++;
+//   }
+// 
+//   return out_p - out;
+// }
